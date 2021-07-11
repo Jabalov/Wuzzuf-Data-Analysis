be8344d41ed794aa2dfddf6017b54ed286e0f5d9
jabalov <moh.aboajabl@gmail.com>
2021-07-11 22:12:21 +0200

    Uploading..

---
+++ src/main/java/com/wuzzuf/analysis/Business/PojoClass.java
@@ -0,0 +1,39 @@
+package com.wuzzuf.analysis;
+
+public class PojoClass
+{
+    private String Title;
+    private String Company;
+    private String Location;
+    private String Type;
+    private String Level;
+    private String YeasExp;
+    private String Country;
+    private String Skills;
+
+    public String getTitle() {
+        return Title;
+    }
+    public String getCompany() {
+        return Company;
+    }
+    public String getLocation() {
+        return Location;
+    }
+    public String getType() {
+        return Type;
+    }
+    public String getLevel() {
+        return Level;
+    }
+    public String getYeasExp() {
+        return YeasExp;
+    }
+    public String getCountry() {
+        return Country;
+    }
+    public String getSkills() {
+        return Skills;
+    }
+
+}
