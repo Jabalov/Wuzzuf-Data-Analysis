13ec2650a282a0309f4fe290aadd16deae113498
asmaa-kashef <58347664+asmaa-kashef@users.noreply.github.com>
2021-07-17 22:17:27 +0200

    Update Controller.java

--- src/main/java/com/wuzzuf/analysis/Controller/Controller.java
+++ src/main/java/com/wuzzuf/analysis/Controller/Controller.java
@@ -20,11 +20,7 @@ public class Controller
    
     private final DAO dao = new DAO();
 
-    @RequestMapping("/show_head_data")
-    public String getHead() { return dao.getHead(); }
-
-    @GetMapping("/show_companies_pie_chart")
-    public  String  show_companies_pie_chart() throws IOException { return dao.getCompanyPieChart(); }
+   
     
 
     @GetMapping("/show_popular_data")
