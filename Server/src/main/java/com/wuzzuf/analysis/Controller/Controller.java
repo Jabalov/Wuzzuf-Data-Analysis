3a7a688c279515df82a8e02cc98c742e4a034d42
jabalov <moh.aboajabl@gmail.com>
2021-07-21 18:52:05 +0200

    Last Update

---
+++ Server/src/main/java/com/wuzzuf/analysis/Controller/Controller.java
@@ -0,0 +1,49 @@
+package com.wuzzuf.analysis.Controller;
+
+import com.wuzzuf.analysis.Business.DAO;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class Controller
+{
+    private final DAO dao = new DAO();
+
+    @GetMapping("/show_popular_titles")
+    public  String PopularTile() throws IOException {  return dao.getPopularTitles();}
+                
+    @GetMapping("/show_titles_pie_chart")
+    public  String  getTitleChart() throws IOException { return dao.getTitlesChart();}
+
+    @GetMapping("/show_head_data")
+    public String getHead() { return dao.getHead(); }
+
+    @GetMapping("/show_most_demanding_companies")
+    public String showMostDamndingCompanies() { return dao.getMostDemandingCompanies(); }
+
+    @GetMapping("/show_companies_pie_chart")
+    public  String  show_companies_pie_chart() throws IOException { return dao.getCompanyPieChart(); }
+
+    @GetMapping("/show_locations_bar_chart")
+    public  String  show_locations_Bar_chart() throws IOException { return dao.getLocationBarChart(); }
+
+    @GetMapping("/show_structure")
+    public @ResponseBody String showStructure() throws IOException { return dao.structure(); }
+  
+    @GetMapping("/show_summary")
+    public String showSummary() throws IOException { return dao.getSummary(); }
+
+    @GetMapping("/show_years_factoreized")
+    public String showYearsFactorized() { return dao.getFactorizedYearsOfExp(); }
+
+    @GetMapping("/apply_kmeans")
+    public String applyKMeans() { return dao.kMeansAlgorithm(); }
+
+    @GetMapping("/get_most_demanded_skills")
+    public @ResponseBody List<Map.Entry> getMostDemandedSkills() { return dao.getMostDemandedSkills(); }
+}
